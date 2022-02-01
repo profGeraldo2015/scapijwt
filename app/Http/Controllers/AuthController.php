4817962905d01542d1002ed332f44f480b34a283
Geraldo <profgeraldofip@gmail.com>
2022-02-01 00:08:33 -0300

    Backand finalizado - token criado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,28 +2,47 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 
-class AuthController extends Controller
-{
+class AuthController extends Controller{
     //
         public function login(LoginRequest $request){
         
-            $input = $request->validate();
-        
-            dd($input);
+            $input = $request->validated();
+
+        //echo var_dump($input);
+
+            //dd($input);
             
-            $credentials = request(['email', 'password']);
+            $credentials = [
+                'email'    => $input['email'],
+                'password' => $input['password'],
+                ];
     
             if (! $token = auth()->attempt($credentials)) {
+
+                //dd($token);
+                
                 return response()->json(['error' => 'Unauthorized'], 401);
+
             }
     
+            //dd($token);
+            //die;
+
             return $this->respondWithToken($token);
         }
-    
-    }
+        protected function respondWithToken($token)
+        {
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ]);
+        }
+        
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\JWTAuth\Contracts\JWTSubject;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
-class User extends Authenticatable implements JWTSubject
-{
+class User extends Authenticatable implements JWTSubject{
+    
     use HasApiTokens, HasFactory, Notifiable;
 
   /**
